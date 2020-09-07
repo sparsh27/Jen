0178585c40ec59dc001e3d2359d972ba57ff62f2
sparsh27 <sparsh0027agarwal@gmail.com>
2020-09-07 20:02:06 +0530

    3rd change

--- Ex1.java
+++ Ex1.java
@@ -3,6 +3,7 @@ public class Ex1{
 public static void main(String[] args){
 
 System.out.println("first change");
-System.out.println("second change"):
+System.out.println("second change");
+System.out.println("third change");
 }
 }
